4e90626919a6a27369468e09f61717b24f95f2dd
Emily Innes <emily.innes@live.com>
2014-09-24 15:55:03 -0230

    working better, still something wrong with the number of vertices being drawn

--- ass1/koch.c
+++ ass1/koch.c
@@ -5,7 +5,8 @@ double width=250.0;
 double height=250.0;
 double x1, x2, x3;
 double y,y2,y3;
-int d=0;
+int n=0;
+int MAX_LEVEL =5;
 void init (void){
 	double angle;
 	glClearColor (1.0,1.0,0.0,0.0);
@@ -54,32 +55,43 @@ void init (void){
   
 }*/
 void drawVertices(double startx, double starty, double endx, double endy, int level){
-	double midx,midy;
+	double x1,y,midx,midy, x2,y2;
 	double len= sqrt(pow((endx-startx),2)+pow((endy-starty),2));
 	//printf("length: %f",len);
 	double angle=atan((endy-starty)/(endx-startx));
-	printf("endy %f starty %f endx %f startx %f", endy, starty, endx, startx);
-	printf("angle %f",angle*180/M_PI);
+	if(angle==-0.0){
+		 angle=M_PI;
+	}
+	//printf("endy %f starty %f endx %f startx %f", endy, starty, endx, startx);
+	//printf("angle %f",angle*180/M_PI);
+	//glVertex3f(startx,starty,0.0);
 	if(level>0){
 		//printf("adding :%f + %f",startx, (len/3.0)*cos(angle*M_PI/180.0));
-		midx=startx+(len/3.0)*cos(angle);
-		midy=starty+(len/3.0)*sin(angle);
-		printf("Point 2: { %f, %f }",midx,midy);
+		
+		x1=startx+(len/3.0)*cos(angle);
+		y=starty+(len/3.0)*sin(angle);
+		drawVertices(startx,starty,x1,y,level-1);
+		//printf("Point 2: { %f, %f }",midx,midy);
+		glVertex3f(x1,y,0.0);
+		//glVertex3f(x1,y,0.0);
+		midx=x1+(len/3.0)*cos((angle+60*M_PI/180));
+		midy=y+(len/3.0)*sin((angle+60*M_PI/180));
+		//printf("Point 3: { %f, %f }",midx,midy);
+		drawVertices(x1,y,midx,midy,level-1);
 		glVertex3f(midx,midy,0.0);
-		glVertex3f(midx,midy,0.0);
-		midx=midx+(len/3.0)*cos((angle+60*M_PI/180));
-		midy=midy+(len/3.0)*sin((angle+60*M_PI/180));
-		printf("Point 3: { %f, %f }",midx,midy);
-		glVertex3f(midx,midy,0.0);
-		glVertex3f(midx,midy,0.0);
-		midx=endx-(len/3.0)*cos(angle);
-		midy=endy-(len/3.0)*sin(angle);
-		printf("Point 4: { %f, %f }",midx,midy);
-		glVertex3f(midx,midy,0.0);
-		glVertex3f(midx,midy,0.0);
+		//glVertex3f(midx,midy,0.0);
+		x2=endx-(len/3.0)*cos(angle);
+		y2=endy-(len/3.0)*sin(angle);
+		//printf("Point 4: { %f, %f }",midx,midy);
+		drawVertices(midx,midy,x2,y2,level-1);
+		glVertex3f(x2,y2,0.0);
+		//glVertex3f(x2,y2,0.0);
+		drawVertices(x2,y2,endx,endy,level-1);
+		
 	}
-	printf("Point 4: { %f, %f }",endx,endy);
-	glVertex3f(endx,endy,0.0);
+	//glVertex3f(endx,endy,0.0);
+	//printf("Point 4: { %f, %f }",endx,endy);
+	//glVertex3f(endx,endy,0.0);
 	
 }
 void display (void){
@@ -87,13 +99,12 @@ void display (void){
   glColor3f(0.0,0.0,1.0);
   //printf("{ %f , %f} { %f , %f} { %f , %f}",x1, y, x2, y, x3, y2);
 	glBegin(GL_LINES);
-		printf("Point 1: { %f, %f }",x1,y);
-		glVertex3f (x1, y, 0.0);
-		drawVertices(x1,y,x3,y3,1);
-		glVertex3f(x3,y3,0.0);
-		drawVertices(x3,y3,x2,y2,1);
-		glVertex3f(x2,y2,0.0);
-		drawVertices(x2,y2,x1,y,1);
+		//printf("Point 1: { %f, %f }",x1,y);	
+		drawVertices(x1,y,x3,y3,n);
+		//glVertex3f(x3,y3,0.0);
+		drawVertices(x3,y3,x2,y2,n);
+		//glVertex3f(x2,y2,0.0);
+		drawVertices(x2,y2,x1,y,n);
 		//glVertex3f(x3,y3,0.0);
 		//drawVertices(x3,y3,x2,y2,1);
 		//glVertex3f(x2,y2,0.0);
@@ -110,11 +121,17 @@ void display (void){
 
 
 void mouse(int btn, int state, int x, int y){
-  if (btn==GLUT_RIGHT_BUTTON && state==GLUT_DOWN) exit(0);
+	if (btn==GLUT_RIGHT_BUTTON && state==GLUT_DOWN) exit(0);
+	if(btn==GLUT_LEFT_BUTTON && state==GLUT_DOWN){
+		n=(n+1)%MAX_LEVEL;
+		printf("level %d", n);
+		glutPostRedisplay();
+	}
 }
 
 void keyboard(unsigned char key, int x, int y){
   if (key =='Q' | key =='q') exit(0);
+
 }
 void reshape(GLsizei w, GLsizei h){
 glMatrixMode(GL_PROJECTION);
