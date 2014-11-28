3774ece6b8f6ed0cb4b25653ea09a4664f4d7483
Emily <emily.innes@live.com>
2014-11-28 15:05:24 -0330

    More stuff

--- ass4/q1.c
+++ ass4/q1.c
@@ -1,14 +1,5 @@
-/* Demo.cpp
-* A simple OpenGL program using GLUT.  It creates a blank black
-* window which can be used as a starting point for OpenGL programming.
-*
-* Link in opengl32, glu32, and glut32 libraries and make sure to include
-* windows.h if you are compiling with Eclipse in Windows.
-*
-* Author: Paul Solt  3-4-07
-* Based on examples from the Red book OpenGL Programming Guide
-*/
-#include <windows.h>
+
+//#include <windows.h>
 #include <GL/glut.h>
 
 const int WIDTH = 600;
@@ -18,69 +9,62 @@ const int HEIGHT = 480;
 void init();
 void display();
 
-/* Definitions */
 
-
-
-GLfloat tailPts[4][4][3] = {
-		{ { -0.25, 0, 1.5 }, { -0.125, 0, 2 }, { 0.375, 0, 2.5 }, { 0, 0, 3 } },
-		{ { -0.125, 0, 1.5 }, { 0, 0, 2 }, { 0.5, 0, 2.5 }, { 0, 0, 3 } },
-		{ { 0.125, 0, 1.5 }, { 0.375, 0, 2 }, { 0.375, 0, 2.5 }, { 0, 0, 3 } },
-		{ { 0.25, 0, 1.5 }, { 0.5, 0, 2 }, { 0.5, 0, 2.5 }, { 0, 0, 3 } }
-
-};
-
+//variable for the viewing angle
 static GLfloat angle[] = { 0, 45, 0 };
 
-
+//variable for the camera's x position
 double cameraX = 0;
+
+//variable for the camera's y position
 double cameraY = 0;
-double cameraZoom = 0;
-GLfloat armHeight = 0.0;
-GLfloat perStep = -.02;
-GLfloat waggle = 0.0;
-GLfloat wagPS = -0.04;
-void updatePerspective(){
+
+//variable for the camera's z position
+double cameraZ = 1;
+
+/* Function for updating the perspective*/
+void updatePerspective(int w, int h){
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluPerspective(60.0, 0.8, 0.1, 20);
+	gluPerspective(60.0,w/h, 0.1, 20);
 }
+
 void
 initlights(void)
 {
-	GLfloat ambient[] = { 0.2, 0.2, 0.2, 1.0 };
-	GLfloat position[] = { 0.0, 0.0, 2.0, 1.0 };
-	GLfloat mat_diffuse[] = { 0.3, 0.6, 0.1, 1.0 };
-	GLfloat mat_specular[] = { 0.0, 1.0, 1.0, 1.0 };
-	GLfloat mat_shininess[] = { 50.0 };
+    GLfloat ambient[] = {0.2, 0.2, 0.2, 1.0};
+    GLfloat position[] = {0.0, 3, 0.5, 1.0};
+    GLfloat mat_diffuse[] = {1.0, 1.0, 1.00, 1.0};
+    GLfloat mat_specular[] = {1.0, 1.0, 1.0, 1.0};
+    GLfloat mat_shininess[] = {50.0};
 
-	glEnable(GL_LIGHTING);
-	glEnable(GL_LIGHT0);
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
 
-	glLightfv(GL_LIGHT0, GL_AMBIENT, ambient);
-	glLightfv(GL_LIGHT0, GL_POSITION, position);
+    glLightfv(GL_LIGHT0, GL_AMBIENT, ambient);
+    glLightfv(GL_LIGHT0, GL_POSITION, position);
 
-	glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
-	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
-	glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
+    glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+    glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
 }
-
-
-void
-myinit(void)
-{
+void init(void){
 	glClearColor(0.0, 0.0, 0.0, 1.0);
 	glEnable(GL_DEPTH_TEST);
-	glEnable(GL_FLAT);
+	//initlights();
 }
+
+/* Function for drawing the cone */
 void drawCone(){
 	GLfloat conePoints[4][4][3] = {
-			{ { 0, -1.0, 1 }, { 0, 0, 0.8 }, { 0, 4.0, 0 }, { 0, 4.0, 0 } },
-			{ { .8, -1.0, .8 }, { 0, 0, 0.6 }, { 0, 4.0, 0 }, { 0, 4.0, 0 } },
-			{ { .8, -1.0, -.8 }, { 0.6, 0, -0.6 }, { 0, 4.0, 0 }, { 0, 4.0, 0 } },
-			{ { 0, -1.0, -1.0 }, { 0, 0, -0.8 }, { 0, 4.0, 0 }, { 0, 4.0, 0 } }
+		{ { 0, -1.0, 1 }, { 0, 0, 0.8 }, { 0, 4.0, 0 }, { 0, 4.0, 0 } },
+		{ { .8, -1.0, .8 }, { 0, 0, 0.6 }, { 0, 4.0, 0 }, { 0, 4.0, 0 } },
+		{ { .8, -1.0, -.8 }, { 0.6, 0, -0.6 }, { 0, 4.0, 0 }, { 0, 4.0, 0 } },
+		{ { 0, -1.0, -1.0 }, { 0, 0, -0.8 }, { 0, 4.0, 0 }, { 0, 4.0, 0 } }
 
 	};
+
+	glColor4f(85/256.0, 0, 0, 1);
 	glMap2f(GL_MAP2_VERTEX_3, 0, 1, 3, 4, 0, 1, 12, 4, &conePoints[0][0][0]);
 	glEnable(GL_MAP2_VERTEX_3);
 	glEnable(GL_AUTO_NORMAL);
@@ -88,6 +72,7 @@ void drawCone(){
 	glMapGrid2f(20, 0.0, 1.0, 20, 0.0, 1.0);
 	glEvalMesh2(GL_FILL, 0, 20, 0, 20);
 	glPushMatrix();
+	glColor4f(125/256.0, 19/256.0, 19/256.0, 1);
 	glRotatef(180.0, 0, 1.0, 0.0);
 	glMap2f(GL_MAP2_VERTEX_3, 0, 1, 3, 4, 0, 1, 12, 4, &conePoints[0][0][0]);
 	glEnable(GL_MAP2_VERTEX_3);
@@ -97,6 +82,7 @@ void drawCone(){
 	glEvalMesh2(GL_FILL, 0, 20, 0, 20);
 	glPopMatrix();
 }
+/* Function for drawing 1/4 of the sphere */
 void sphereSegment(){
 	GLfloat spherePoints[4][4][3] = {
 			{ { 0, 1, 0 }, { 0, 1, 0.2 }, { 0, 1, 0.8 }, { 0, 0, 1 } },
@@ -111,28 +97,35 @@ void sphereSegment(){
 	glMapGrid2f(20, 0.0, 1.0, 20, 0.0, 1.0);
 	glEvalMesh2(GL_FILL, 0, 20, 0, 20);
 }
+
+/* Function for drawing a full sphere */
 void drawSphere(){
+	glColor4f(34/256.0,102/256.0, 102/256.0, 1.0);
 	sphereSegment();
 	glPushMatrix();
+	glColor4f(46/256.0,66/256.0,114/256.0,1.0);
 	glRotatef(180.0, 0.0, 1.0, 0.0);
 	sphereSegment();
 	glPopMatrix();
 	glPushMatrix();
+	glColor4f(45/256.0,136/256.0,45/256.0,1.0);
 	glRotatef(180.0, 1.0, 0, 0.0);
 	sphereSegment();
 	glPopMatrix();
 	glPushMatrix();
-	glRotatef(180.0, 1.0, 0.0, 0.0);
+	glColor4f(34/256.0,102/256.0, 102/256.0, 1.0);
+	glRotatef(180.0, 0.0, 0.0, 1.0);
 	sphereSegment();
 	glPopMatrix();
 
 }
+/* Function to draw a plane */
 void drawPlane(GLfloat warp){
 	GLfloat planePoints[4][4][3] = {
-			{ { 1, -1, -1 }, { 1, -1 + warp, 0 }, { 1, -1 + warp, 0 }, { 1, -1, 1 } },
-			{ { 0.5, -1, -1 }, { 0.5, -1 + warp, 0 }, { 0.5, -1 + warp, 0 }, { 0.5, -1, 1 } },
-			{ { -0.5, -1, -1 }, { -0.5, -1 + warp, 0 }, { -0.5, -1 + warp, 0 }, { -0.5, -1, 1 } },
-			{ { -1, -1, -1 }, { -1, -1 + warp, 0 }, { -1, -1 + warp, 0 }, { -1, -1, 1 } }
+		{ { 1, -1, -1 }, { 1, -1 + warp, 0 }, { 1, -1 + warp, 0 }, { 1, -1, 1 } },
+{ { 0.5, -1, -1 }, { 0.5, -1 + warp, 0 }, { 0.5, -1 + warp, 0 }, { 0.5, -1, 1 } },
+{ { -0.5, -1, -1 }, { -0.5, -1 + warp, 0 }, { -0.5, -1 + warp, 0 }, { -0.5, -1, 1 } },
+	{ { -1, -1, -1 }, { -1, -1 + warp, 0 }, { -1, -1 + warp, 0 }, { -1, -1, 1 } }
 	};
 	glMap2f(GL_MAP2_VERTEX_3, 0, 1, 3, 4, 0, 1, 12, 4, &planePoints[0][0][0]);
 	glEnable(GL_MAP2_VERTEX_3);
@@ -141,57 +134,63 @@ void drawPlane(GLfloat warp){
 	glMapGrid2f(20, 0.0, 1.0, 20, 0.0, 1.0);
 	glEvalMesh2(GL_FILL, 0, 20, 0, 20);
 }
+/* Function to draw a slightly curved cube */
 void drawCube(){
+	glColor4f(0/256.0,64/256.0,8/256.0,1.0);
 	drawPlane(-0.1);
 	glPushMatrix();
 	glRotatef(180, 0, 0, 1.0);
+	glColor4f(0/256.0,64/256.0,8/256.0,1.0);
 	drawPlane(-0.1);
 	glPopMatrix();
 
 	glPushMatrix();
 	glRotatef(90, 1.0, 0, 0.0);
+	glColor4f(137/256.0,199/256.0,145/256.0,1.0);
 	drawPlane(-0.1);
 	glPopMatrix();
 	glPushMatrix();
 	glRotatef(-90.0, 1.0, 0, 0);
+	glColor4f(85/256.0,166/256.0,95/256.0,1.0);
 	drawPlane(-0.1);
 	glPopMatrix();
 	glPushMatrix();
 	glRotatef(-90.0, 0, 0, 1.0);
+	glColor4f(44/256.0,132/256.0,55/256.0,1.0);
 	drawPlane(-0.1);
 	glPopMatrix();
 	glRotatef(90.0, 0, 0, 1.0);
+	glColor4f(15/256.0,98/256.0,25/256.0,1.0);
 	drawPlane(-0.1);
 	glPopMatrix();
 
 }
-void
-display(void)
+
+void display(void)
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	// glMatrixMode(GL_PROJECTION);
-	//gluPerspective(3,1,-10,10);
 
+	glEnable(GL_FLAT);
+	glShadeModel(GL_FLAT);
 	glPushMatrix();
 	glRotatef(angle[0], 1.0, 0.0, 0.0);
 	glRotatef(angle[1], 0.0, 1.0, 0.0);
 	glRotatef(angle[2], 0.0, 0.0, 1.0);
-	//glScalef(2,2,2);
-	glTranslatef(1 - cameraZoom, 0.7, -1 + cameraZoom);
+	glTranslatef(cameraZ, 0.7,  -1*cameraZ);
 
+	// floor
 	glPushMatrix();
-	//green floor
-	glColor4f(0, 1.0, 0, 1.0);
+	glColor4f(2/256.0, 12/256.0, 34/256.0, 1.0);
 	drawPlane(0);
-	glColor4f(1.0, 0, 0, 1.0);
+	//left wall
+	glColor4f(6/256.0, 21/256.0, 57/256.0, 1.0);
 	glPushMatrix();
-	//red wall
 	glRotatef(90, 1.0, 0, 0);
 	drawPlane(0);
 	glPopMatrix();
-	glColor4f(0.0, 0, 1.0, 1.0);
+	//right wall
+	glColor4f(20/256.0, 39/256.0, 83/256.0, 1.0);
 	glPushMatrix();
-	//blue wall
 	glRotatef(90, 0, 0, 1.0);
 	drawPlane(0);
 	glPopMatrix();
@@ -202,46 +201,34 @@ display(void)
 	glColor4f(0.688, 0.422, 0.223, 1.0);
 	drawCube();
 	glPopMatrix();
+	//transformation for the sphere
 	glPushMatrix();
 	glTranslatef(-.375, -.85, -.66);
 	glScalef(.1, .1, .1);
-	glColor4f(0.178, 0.258, 0.445, 1.0);
 	drawSphere();
 	glPopMatrix();
+	//transformation for the cone
 	glPushMatrix();
 	glTranslatef(-.4, -1, .90);
 	glScalef(.1, .1, .1);
-	glColor4f(.133, .399, .399, .1);
 	drawCone();
 	glPopMatrix();
 	glPopMatrix();
-
 	glPopMatrix();
 	glFlush();
 	glutSwapBuffers();
 	glutPostRedisplay();
 }
+/* Keyboard Function. WS to move along the main diagonal of the scene, AD to look around*/
 void keyboard(unsigned char key, int x, int y){
-	/*****WASDQE for rotation about the relevant axis******/
-	if (key == 'W' | key == 'w'){
-		//switch(mode){
-		//case(0):
-		/*if(angle[0]>=360.0)angle[0]=0;
-		angle[0]+=3.0;*/
-		cameraZoom += 0.03;
-		//break;
-		//}
 
+	if (key == 'W' | key == 'w'){
+		if(cameraZ<=-1){}
+		else cameraZ -= 0.03;
 	}
 	if (key == 'S' | key == 's'){
-		//switch(mode){
-		//case(0):
-		//if(angle[0]<=0)angle[0]=360;
-		//	angle[0]-=3.0;
-		cameraZoom -= 0.03;
-		//break;
-
-		//}
+		if(cameraZ>=1){}
+		else cameraZ += 0.03;
 	}
 
 	if (key == 'D' | key == 'd'){
@@ -250,55 +237,19 @@ void keyboard(unsigned char key, int x, int y){
 
 	}
 	if (key == 'A' | key == 'a'){
-
-
 		if (angle[1] <= 0)angle[1] = 360;
 		angle[1] -= 3.0;
 
 	}
-	if (key == 'Q' | key == 'q'){
-		//switch(mode){
-		//case(0):
-		if (angle[2] >= 360.0)angle[2] = 0;
-		angle[2] += 3.0;
-		//break;
-		//}
-	}
-	if (key == 'E' | key == 'e'){
-		//switch(mode){
-		//case(0):
-		if (angle[2] <= 0)angle[2] = 360;
-		angle[2] -= 3.0;
-		//break;
-		//}
-	}
-
-
-
-
-
 	glutPostRedisplay();
 
 }
 
-/*void setProjectionMatrix (int width, int height)
-{
-glMatrixMode(GL_PROJECTION);
-glLoadIdentity();
-gluPerspective (50.0*cameraZoom, (float)width/(float)height, -5, 5);
-}*/
 void
 myReshape(int w, int h)
 {
 	glViewport(0, 0, w, h);
-	updatePerspective();
-	/*  if (w <= h)
-	glOrtho(-4.0, 4.0, -4.0 * (GLfloat) h / (GLfloat) w,
-	4.0 * (GLfloat) h / (GLfloat) w, -4.0, 4.0);
-	else
-	glOrtho(-4.0 * (GLfloat) w / (GLfloat) h,
-	4.0 * (GLfloat) w / (GLfloat) h, -4.0, 4.0, -4.0, 4.0);*/
-
+	updatePerspective(w,h);
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 }
@@ -308,8 +259,10 @@ main(int argc, char **argv)
 {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-	glutCreateWindow(argv[0]);
-	myinit();
+	glutCreateWindow("Assignment 4 Scene");
+	glutInitWindowSize(WIDTH,HEIGHT);
+	glutInitWindowPosition(100,100);
+	init();
 	glutReshapeFunc(myReshape);
 	glutDisplayFunc(display);
 	glutKeyboardFunc(keyboard);
--- ass4/q2.c
+++ ass4/q2.c
@@ -1,16 +1,11 @@
-/* Demo.cpp
- * A simple OpenGL program using GLUT.  It creates a blank black
- * window which can be used as a starting point for OpenGL programming.
- *
- * Link in opengl32, glu32, and glut32 libraries and make sure to include
- * windows.h if you are compiling with Eclipse in Windows.
- *
- * Author: Paul Solt  3-4-07
- * Based on examples from the Red book OpenGL Programming Guide
- */
-#include <windows.h>
-#include <GL/glut.h>
+/**************
+This is some kind of stingray/eel hybrid. Stingrays don't actually move their tails when
+they swim, but I thought the tail waggle was cute
+**************/
 
+//#include <windows.h>
+#include <stdio.h>
+#include <GL/glut.h>
 const int WIDTH = 600;
 const int HEIGHT = 480;
 
@@ -35,23 +30,28 @@ GLfloat tailPts[4][4][3]={
 
 };
 
-static GLfloat angle[]={20.0,45.0,25.0};
+static GLfloat angle[]={21,108,0};
+static GLfloat pos[]={-1,-0.8,-4};
 
 
-double cameraX=0;
-double cameraY=0;
-double cameraZoom=0;
 GLfloat armHeight=0.0;
 GLfloat perStep= -.02;
 GLfloat waggle=0.0;
 GLfloat wagPS=-0.04;
+
+/* Function for updating the perspective*/
+void updatePerspective(int w, int h){
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(60.0,w/h, 0.1, 20);
+}
 void
-initlights(void)
+initlights(double r, double g, double b)
 {
-    GLfloat ambient[] = {0.2, 0.2, 0.2, 1.0};
-    GLfloat position[] = {0.0, 0.0, 2.0, 1.0};
-    GLfloat mat_diffuse[] = {0.3, 0.6, 0.1, 1.0};
-    GLfloat mat_specular[] = {0.0, 1.0, 1.0, 1.0};
+    GLfloat ambient[] = {1, 1, 1, 1.0};
+    GLfloat position[] = {0.0, 0.2, 3.0, 1.0};
+    GLfloat mat_diffuse[] = {r,g, b, 1.0};
+    GLfloat mat_specular[] = {1.0, 1.0, 1.0, 1.0};
     GLfloat mat_shininess[] = {50.0};
 
     glEnable(GL_LIGHTING);
@@ -81,7 +81,21 @@ void updateStingray(){
 		tailPts[i][2][3]-=wagPS;
 	}
 }
+void drawStingray(){
+	glMap2f(GL_MAP2_VERTEX_3, 0, 1, 3, 4, 0, 1, 12, 4, &ctrlpts[0][0][0]);
+	glEnable(GL_MAP2_VERTEX_3);
+	glEnable(GL_AUTO_NORMAL);
+	glEnable(GL_NORMALIZE);
+	glMapGrid2f(20, 0.0, 1.0, 20, 0.0, 1.0);
+	glEvalMesh2(GL_FILL, 0, 20, 0, 20);
+	glMap2f(GL_MAP2_VERTEX_3, 0, 1, 3, 4, 0, 1, 12, 4, &tailPts[0][0][0]);
+	glEnable(GL_MAP2_VERTEX_3);
+	glEnable(GL_AUTO_NORMAL);
+	glEnable(GL_NORMALIZE);
+	glMapGrid2f(20, 0.0, 1.0, 20, 0.0, 1.0);
+	glEvalMesh2(GL_FILL, 0, 20, 0, 20);
 
+}
 void
 myinit(void)
 {
@@ -93,115 +107,101 @@ myinit(void)
    // glEnable(GL_AUTO_NORMAL);
    // glEnable(GL_NORMALIZE);
   //  glMapGrid2f(20, 0.0, 1.0, 20, 0.0, 1.0);
-   initlights();       /* for lighted version only */
+   //initlights();       /* for lighted version only */
 }
 void
 display(void)
 {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glColor4f(1.0,0.0,1.0,1.0);
-    glPushMatrix();
-    glRotatef(angle[0],1.0,0.0,0.0);
+	glMatrixMode(GL_MODELVIEW);
+	    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glColor4f(1.0,0.0,1.0,1.0);
+	glPushMatrix();
+
+	glTranslatef(pos[0],pos[1],pos[2]);
+	glRotatef(angle[0],1.0,0.0,0.0);
 	glRotatef(angle[1],0.0,1.0,0.0);
 	glRotatef(angle[2],0.0,0.0,1.0);
-    //glRotatef(85.0, 1.0, 1.0, 1.0);
-	glMap2f(GL_MAP2_VERTEX_3, 0, 1, 3, 4, 0, 1, 12, 4, &ctrlpts[0][0][0]);
-	glEnable(GL_MAP2_VERTEX_3);
-	glEnable(GL_AUTO_NORMAL);
-	glEnable(GL_NORMALIZE);
-	glMapGrid2f(20, 0.0, 1.0, 20, 0.0, 1.0);
-    glEvalMesh2(GL_FILL, 0, 20, 0, 20);
-	glMap2f(GL_MAP2_VERTEX_3, 0, 1, 3, 4, 0, 1, 12, 4, &tailPts[0][0][0]);
-	glEnable(GL_MAP2_VERTEX_3);
-	glEnable(GL_AUTO_NORMAL);
-	glEnable(GL_NORMALIZE);
-	glMapGrid2f(20, 0.0, 1.0, 20, 0.0, 1.0);
-    glEvalMesh2(GL_FILL, 0, 20, 0, 20);
-    glPopMatrix();
-    updateStingray();
-    glFlush();
-    glutSwapBuffers();
-    glutPostRedisplay();
+	/*glPushMatrix();
+	glTranslatef(1,1.5,-1);
+	glScalef(1.5,1.5,1.5);
+	initlights(.8,.4,.6);
+	drawStingray();
+	glPopMatrix();
+	glTranslatef(0,-.5,0);
+	initlights(.5,.6,.7);
+	drawStingray();
+	glPopMatrix();*/
+	initlights(.5,.6,.7);
+	drawStingray();
+	glPopMatrix();
+	updateStingray();
+	glFlush();
+	glutSwapBuffers();
+	glutPostRedisplay();
 }
 void keyboard(unsigned char key, int x, int y){
-/*****WASDQE for rotation about the relevant axis******/
+/*****WASDQE for movement ******/
 	if (key =='W' | key =='w'){
-		//switch(mode){
-			//case(0):
-				if(angle[0]>=360.0)angle[0]=0;
-				angle[0]+=3.0;
-			//break;
-		//}
+		pos[1]-=0.1;
+				
+
 
 	}
 	if( key=='S'| key=='s'){
-		//switch(mode){
-			//case(0):
-			if(angle[0]<=0)angle[0]=360;
-			angle[0]-=3.0;
-			//break;
+		pos[1]+=0.1;
 
-		//}
 	}
 
 	if (key =='D' | key =='d'){
-			if(angle[1]>=360.0)angle[1]=0;
-			angle[1]+=3.0;
+		pos[0]-=0.1;
 
 	}
 	if (key =='A' | key =='a'){
-
-
-			if(angle[1]<=0)angle[1]=360;
-			angle[1]-=3.0;
+		pos[0]+=0.1;
 
 	}
 	if (key =='Q' | key =='q'){
-		//switch(mode){
-			//case(0):
-			if(angle[2]>=360.0)angle[2]=0;
-			angle[2]+=3.0;
-			//break;
-		//}
+		pos[2]+=0.1;
 	}
 	if (key =='E' | key =='e'){
-		//switch(mode){
-			//case(0):
-			if(angle[2]<=0)angle[2]=360;
-			angle[2]-=3.0;
-			//break;
-		//}
+		pos[2]-=0.1;
 	}
-	/****camera position is controlled with ijkl, zoom with YU****/
+	/****Rotate the camera with IJKLUO****/
 	if (key =='I' | key =='i'){
-
-				cameraY-=0.05;
+		if(angle[0]>=360.0)angle[0]=0;
+			angle[0]+=3.0;
 
 
 	}
 	if( key=='K'| key=='k'){
-
-			cameraY+=0.05;
+		if(angle[0]<=0)angle[0]=360;
+			angle[0]-=3.0;
 
 
 	}
 
 	if (key =='J' | key =='j'){
-
-
-			cameraX+=0.05;
+		if(angle[1]<=0)angle[1]=360;
+		angle[1]-=3.0;
 
 	}
 	if (key =='L' | key =='l'){
+		if(angle[1]>=360.0)angle[1]=0;
+			angle[1]+=3.0;
 
-			cameraX-=0.05;
 
 	}
 	if(key=='U' | key=='u'){
-		cameraZoom+=0.05;
+		if(angle[2]>=360.0)angle[2]=0;
+			angle[2]+=3.0;
 	}
-	if(key=='Y'| key=='y') cameraZoom-=0.05;
-
+	if(key=='O'| key=='o'){
+		if(angle[2]<=0)angle[2]=360;
+			angle[2]-=3.0;
+	}
+	if(key=='m'){
+		printf("Camera position x %f y %f z%f rotation x %f y %f z%f",pos[0],pos[1],pos[2],angle[0],angle[1],angle[2]);
+	}
 	glutPostRedisplay();
 
 }
@@ -210,17 +210,10 @@ void keyboard(unsigned char key, int x, int y){
 void
 myReshape(int w, int h)
 {
-    glViewport(0, 0, w, h);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    if (w <= h)
-        glOrtho(-4.0, 4.0, -4.0 * (GLfloat) h / (GLfloat) w,
-            4.0 * (GLfloat) h / (GLfloat) w, -4.0, 4.0);
-    else
-        glOrtho(-4.0 * (GLfloat) w / (GLfloat) h,
-            4.0 * (GLfloat) w / (GLfloat) h, -4.0, 4.0, -4.0, 4.0);
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
+	glViewport(0, 0, w, h);
+	updatePerspective(w,h);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
 }
 
 int
